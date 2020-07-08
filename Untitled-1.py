f16438eb50c32f62dd83a47ff2897b128a9bb294
Nickkodag <53111448+Nickkodag@users.noreply.github.com>
2020-07-08 17:23:46 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+import random
+dice1 =random.randrange(1,7)
+dice2 =random.randrange(1,7)
+print("dice1:",dice1,"dice2:",dice2,)
+
+
+
+import random
+dice1 =random.randrange(1,7)
+for i in range(2):
+    dice =random.randrange(1,7)
+    print("dice1:",dice)
